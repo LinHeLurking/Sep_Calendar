99e6c290a63e18d8b7a55ce8bae201b028b604aa
LinHeLurking <LinHe.Lurking@gmail.com>
2022-02-21 19:13:07 +0800

    update version number

--- sel.user.js
+++ sel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SEP Calendar Genterator
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.js
 // @description  Generate calendar from SEP!
 // @author       LinHeLurking
